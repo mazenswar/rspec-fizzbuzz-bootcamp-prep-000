ab281651c90d57a57201c8bf8f9ee2145355281d
Mazen AlSwar <mazenswar@protonmail.com>
2018-10-18 19:42:05 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,10 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def fizbuzz(num)
+  if num % 3 == 0 
+    puts "fizz"
+  elsif num % 5 == 0 
+    puts "buzz"
+  elsif num % 3 == 0 && % 5 == 0  
+end
