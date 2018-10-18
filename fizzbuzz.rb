cf074f13c3aec3b13d51b896a8cf64739184878b
Mazen AlSwar <mazenswar@protonmail.com>
2018-10-18 19:56:04 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,11 +3,11 @@
 
 def fizzbuzz(num)
   if num % 3 == 0 
-    puts "Fizz"
+    "Fizz"
   elsif num % 5 == 0 
-    puts "Buzz"
+    "Buzz"
   elsif num % 3 == 0 && num % 5 == 0
-    puts "FizBuzz"
+    "FizBuzz"
   else
     return nil 
   end
